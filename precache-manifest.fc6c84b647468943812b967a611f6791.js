a8e972b2fcf4ec8793555f0767a0e2f92aadffbf
HakonArnarS <43438456+HakonArnarS@users.noreply.github.com>
2019-11-26 09:40:59 +0000

    removed dist from gitignore

---
+++ precache-manifest.fc6c84b647468943812b967a611f6791.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ca8b5d499c45a1748f",
+    "url": "css/app.60fb4d25.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "ce9b05a187350786b0d4a243f428e8c4",
+    "url": "index.html"
+  },
+  {
+    "revision": "f80b0c0dffd12376769e",
+    "url": "js/280aa2ca.24c01271.js"
+  },
+  {
+    "revision": "6556ac29e2705dd05f30",
+    "url": "js/2d0b1f69.f2677849.js"
+  },
+  {
+    "revision": "3f64917c4d9e995bab94",
+    "url": "js/2d0ccf5d.c3ec3d28.js"
+  },
+  {
+    "revision": "b32b36af7a1770d90389",
+    "url": "js/2d0e8be2.0c31e73e.js"
+  },
+  {
+    "revision": "b739071b783b708578ae",
+    "url": "js/7089f5c5.2e34eeda.js"
+  },
+  {
+    "revision": "f06bdf1656fc52f8c371",
+    "url": "js/830425fa.2589d56f.js"
+  },
+  {
+    "revision": "cc90e5e0ff200dfe8b05",
+    "url": "js/832c488c.3f534377.js"
+  },
+  {
+    "revision": "7a1b06aa07ff389842f8",
+    "url": "js/a04e089a.e03ec149.js"
+  },
+  {
+    "revision": "16ca8b5d499c45a1748f",
+    "url": "js/app.a01972df.js"
+  },
+  {
+    "revision": "eb7b17e80a6b0f0b62e0",
+    "url": "js/fastclick.5251028d.js"
+  },
+  {
+    "revision": "0991309a1eb9027d4abf",
+    "url": "js/vendor.91ad30d7.js"
+  },
+  {
+    "revision": "af0cb15c0f16b4e6559d89818aa892ed",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "bde75c95c26cbe407d8c3a51149c9047",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1e6959a9778d40656217fe5eb7e0b7aa",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "e776afac4479ee2ad8d6a0397661bf09",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "37ab9045dbdc0edf12ea59b3772e19f9",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "65d8571ec508d854ae4132e6540c476a",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7233d85b329c161211830b1b30fdf0ec",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c7428f8eba0499407710120fea46ead7",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "04671eaea0c2fc94e31c5ab76ec67d08",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "3f5de7654b25e5f1640662111d28b490",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "15bc9eca3421a7d5a0599a5fb39fe7a8",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "f309ab32f7ba3a2d3f50d05ba3d2aaa2",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "c8fdfe20437ccd115ee07cb684685ded",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "6624870bb64b1a562e8438e57cdc636a",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "ba58d8b825312a31114fb91484dadbeb",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "edff3dcaca87028d9217a89e93e8c3ff",
+    "url": "statics/starbucks-128x128.png"
+  },
+  {
+    "revision": "684a742e032dae84395a86c5630525c9",
+    "url": "statics/starbucks-16x16.png"
+  },
+  {
+    "revision": "a05e79e4ba178fcf32dcd7dfd251538d",
+    "url": "statics/starbucks-192x192.png"
+  },
+  {
+    "revision": "f349c3eb122feeb9f057bbfec527c5cb",
+    "url": "statics/starbucks-256x265.png"
+  },
+  {
+    "revision": "351eefb4f9035307f3958547fd67122b",
+    "url": "statics/starbucks-32x32.png"
+  },
+  {
+    "revision": "c5c729fd9fcc4004a28c589120e23022",
+    "url": "statics/starbucks-384x384.png"
+  },
+  {
+    "revision": "4825a0be565ece549738be36ea8aa067",
+    "url": "statics/starbucks-512x512.png"
+  },
+  {
+    "revision": "e0dd126242f08728a21a6c102231a3d1",
+    "url": "statics/starbucks-96x96.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,32 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.fc6c84b647468943812b967a611f6791.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "quasar-test"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
