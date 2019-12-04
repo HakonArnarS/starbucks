230697a6a264779fb79043976f35f58752ce794c
Robo Cop <robot@example.com>
2019-12-04 14:50:36 +0000

    Updates

---
+++ precache-manifest.31f6ea47e774af15b092fa55404c6819.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df5c3cd449a61315a64",
+    "url": "/starbucks/css/app.833f6c58.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/starbucks/fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/starbucks/fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/starbucks/fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/starbucks/fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/starbucks/fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/starbucks/fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/starbucks/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/starbucks/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "a65597457cc9cb5118393b4059774610",
+    "url": "/starbucks/img/happyhour.a6559745.jpg"
+  },
+  {
+    "revision": "47ff98a7daf49238e6746d89f03e67b3",
+    "url": "/starbucks/index.html"
+  },
+  {
+    "revision": "2d6b40025c18499af40a",
+    "url": "/starbucks/js/076faf7a.6632e487.js"
+  },
+  {
+    "revision": "cbbb79020c2b9820712a",
+    "url": "/starbucks/js/280aa2ca.14b0268d.js"
+  },
+  {
+    "revision": "9ada85927dfb8479c1f8",
+    "url": "/starbucks/js/2d0ac1df.2091332b.js"
+  },
+  {
+    "revision": "872edb705d6dc95e0137",
+    "url": "/starbucks/js/2d0b1f69.58e1bac1.js"
+  },
+  {
+    "revision": "3f64917c4d9e995bab94",
+    "url": "/starbucks/js/2d0ccf5d.c3ec3d28.js"
+  },
+  {
+    "revision": "b739071b783b708578ae",
+    "url": "/starbucks/js/7089f5c5.2e34eeda.js"
+  },
+  {
+    "revision": "f06bdf1656fc52f8c371",
+    "url": "/starbucks/js/830425fa.2589d56f.js"
+  },
+  {
+    "revision": "47f9acbef4f625335ea1",
+    "url": "/starbucks/js/832c488c.7b8ccecc.js"
+  },
+  {
+    "revision": "2df5c3cd449a61315a64",
+    "url": "/starbucks/js/app.3dc8a185.js"
+  },
+  {
+    "revision": "b088cca66a25e29551e2",
+    "url": "/starbucks/js/f766676e.bcac86ee.js"
+  },
+  {
+    "revision": "eb7b17e80a6b0f0b62e0",
+    "url": "/starbucks/js/fastclick.5251028d.js"
+  },
+  {
+    "revision": "fc77436bc61d0faa9f54",
+    "url": "/starbucks/js/vendor.90e4f8d4.js"
+  },
+  {
+    "revision": "d4017551ba167a7c0d35b4cd47812a05",
+    "url": "/starbucks/manifest.json"
+  },
+  {
+    "revision": "ae154dc7ae61bb47ff623b56183b7455",
+    "url": "/starbucks/statics/app-icon.png"
+  },
+  {
+    "revision": "a5259e7e1cebd53e01aaa7e44bad0977",
+    "url": "/starbucks/statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "a65597457cc9cb5118393b4059774610",
+    "url": "/starbucks/statics/happyhour.jpg"
+  },
+  {
+    "revision": "55e28ce248ff8544c2b792751c7bfef1",
+    "url": "/starbucks/statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "5e6a4a343335b7b56ff9f19e5b2382fd",
+    "url": "/starbucks/statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "ae6e6be1210d03557c6b4c17d9e548a2",
+    "url": "/starbucks/statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "d39f2a30f16d043a34712da5a2c26f78",
+    "url": "/starbucks/statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "88090d237807da1ceca89c6059c14b5a",
+    "url": "/starbucks/statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "3ac9fbfbc141cbfe91afb31e51d9e1fb",
+    "url": "/starbucks/statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "756bfdd98a4c5d8d5e775a02adcba862",
+    "url": "/starbucks/statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "35bdd37939af3170e4facf135d75b529",
+    "url": "/starbucks/statics/icons/favicon.ico"
+  },
+  {
+    "revision": "a5259e7e1cebd53e01aaa7e44bad0977",
+    "url": "/starbucks/statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "6484e15e2fcac8c0931249eb21aadd8f",
+    "url": "/starbucks/statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "36bd8861be84757110ac681e8a304c08",
+    "url": "/starbucks/statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "5c840947ec8e36191644ede6e3994877",
+    "url": "/starbucks/statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "d0ccfdcb7dee0171d8cbe1d7be6a5b6a",
+    "url": "/starbucks/statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "986875c6e126921a77d4da4408c4e5e2",
+    "url": "/starbucks/statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "c4b42eef92fb273cdb5af31ec015ca86",
+    "url": "/starbucks/statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "edff3dcaca87028d9217a89e93e8c3ff",
+    "url": "/starbucks/statics/icons/starbucks-128x128.png"
+  },
+  {
+    "revision": "684a742e032dae84395a86c5630525c9",
+    "url": "/starbucks/statics/icons/starbucks-16x16.png"
+  },
+  {
+    "revision": "66e3133c36182b842957b1d2c983017d",
+    "url": "/starbucks/statics/icons/starbucks-192x192.png"
+  },
+  {
+    "revision": "f349c3eb122feeb9f057bbfec527c5cb",
+    "url": "/starbucks/statics/icons/starbucks-256x265.png"
+  },
+  {
+    "revision": "351eefb4f9035307f3958547fd67122b",
+    "url": "/starbucks/statics/icons/starbucks-32x32.png"
+  },
+  {
+    "revision": "c5c729fd9fcc4004a28c589120e23022",
+    "url": "/starbucks/statics/icons/starbucks-384x384.png"
+  },
+  {
+    "revision": "4825a0be565ece549738be36ea8aa067",
+    "url": "/starbucks/statics/icons/starbucks-512x512.png"
+  },
+  {
+    "revision": "e0dd126242f08728a21a6c102231a3d1",
+    "url": "/starbucks/statics/icons/starbucks-96x96.png"
+  },
+  {
+    "revision": "edff3dcaca87028d9217a89e93e8c3ff",
+    "url": "/starbucks/statics/starbucks-128x128.png"
+  },
+  {
+    "revision": "8ce12d218d6bf278f9c36719c22ebb28",
+    "url": "/starbucks/statics/starbucks-src1240.svg"
+  }
+]);
